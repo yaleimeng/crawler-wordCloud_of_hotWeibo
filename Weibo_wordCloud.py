af021e65de30cfa4bcf01ad19cb010dbb974cf56
Yalei Meng <yaleimeng@sina.com>
2018-03-29 08:49:12 +0800

    修改输入输出文件路径为相对路径，减少外部影响

--- Weibo_wordCloud.py
+++ Weibo_wordCloud.py
@@ -9,7 +9,7 @@ import jieba
 from wordcloud import WordCloud,ImageColorGenerator
 
 stop = set()
-with open('E:/weibo1010.txt', 'r',encoding='utf-8') as f, open('E:/stopwords.txt', 'r',encoding='utf-8') as s:
+with open('./weibo1010.txt', 'r',encoding='utf-8') as f, open('./stopwords.txt', 'r',encoding='utf-8') as s:
     text = f.read()
     for line in s.readlines():
         if line[:-1] not in stop:
@@ -18,14 +18,14 @@ with open('E:/weibo1010.txt', 'r',encoding='utf-8') as f, open('E:/stopwords.txt
 # 首先使用 jieba 中文分词工具进行分词
 wordlist = (jieba.cut(text, cut_all = False))  # cut_all, True为全模式，False为精确模式
 wordlist_space_split = ' '.join(wordlist)  #使用空格连接区分出来的各个词语。仿照英文的风格。
-src = cv2.imread('D:/hua.jpg')  #图片是生成词云的掩膜。
+src = cv2.imread('./hua.jpg')  #图片是生成词云的掩膜。
 my_wordcloud = WordCloud( font_path='C:/Windows/Fonts/simkai.ttf',
                          background_color='white', max_words=130, mask=src,
                          max_font_size=250, random_state= 130, stopwords= stop,min_font_size=15
                          ).generate(wordlist_space_split)
 image_colors = ImageColorGenerator(src)
 my_wordcloud.recolor(color_func= image_colors)
-fileName = 'E:/new_wb20.png'
+fileName = './new_wb20.png'
 my_wordcloud.to_file(fileName )
 cv2.imshow('word cloud',cv2.imread(fileName))
 cv2.waitKey()
